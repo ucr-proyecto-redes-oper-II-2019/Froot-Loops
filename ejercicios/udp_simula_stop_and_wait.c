2265c58a664974075ef52c0ad57e348070bd1252
Antonio <antonio.alvarez.chavarria@gmail.com>
2019-09-18 20:01:32 -0600

    Restructuración del algoritmo de simulación de tcp con base a lo visto en clase el 17/09/2019

--- ejercicios/udp_simula_stop_and_wait.c
+++ ejercicios/udp_simula_stop_and_wait.c
@@ -22,228 +22,183 @@
 // Driver code
 int main(int argc, char* argv[]) //agrg[1] = my_IP, argv[2] = my_port, argv[3] = receptor_ip, argv[4] = receptor_port, argv[5] = file_path
 {
-	
-	if(argc < 6)
-	{
-		printf("Not enough arguments given\n Usage: ./client <my_IP> <my_port> <receptor_id> <receptor_port> <file_path> \n");
-		return 0;
-	}
-	
-	FILE* file; 
-	
-	file = fopen(argv[5], "rb");
-	/*
-	char* ip;
-	char* send;
-	char* pch;
-	char* other_port;
-	*/
-	char** recv_matrix;
-	pid_t cpid; //create child id
-	int pipefd[2];
-	int batch_counter;
-  int total_package_counter = 0;
-  int total_packages_sent = 0;
-	int ack_chars[PACKAGE_LIMIT];	
-	bzero(ack_chars,PACKAGE_LIMIT);
-	char package[PACK_SIZE];
-	int sockfd, sockfd2, n, len, current_batch_size;
-	struct sockaddr_in me, other;
-	int end_flag = 0;
-	
-	// clear servaddr
-	// Limpia el registro
-	bzero(&me, sizeof(me));
-
-	me.sin_addr.s_addr = inet_addr(argv[1]);
-	me.sin_port = htons(PORT);
-	me.sin_family = AF_INET;
-	
-	other.sin_family = AF_INET;
-	
-	// create datagram socket
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	// bind created socket to my addres
-	bind(sockfd, (struct sockaddr *)&me, sizeof(me));
-
-	int readB = 0;
-	len = sizeof(other);
-	
-	if(pipe(pipefd) == -1)
-	{
-		perror("Could not create pipe\n");
-		exit(EXIT_FAILURE);
-	}
-	
-  current_batch_size = PACKAGE_LIMIT;
     
-	cpid = fork();
-	
-	if(cpid == -1)
-	{
-		perror("Fork failed to create child lul \n");
-		exit(EXIT_FAILURE);
-	}
-	
-	
-	//parent (emisor)
-	if(cpid != 0)
-	{
-      
-    struct stat buffer;
-    int status;
-    status = stat("path to file", &buffer);
-    if(status == 0) {
-      printf("File read succesfully: %zu \n", buffer.st_size);
-         
-      total_package_counter = buffer.st_size / PACK_THROUGHPUT;
+    if(argc < 6)
+    {
+        printf("Not enough arguments given\n Usage: ./client <my_IP> <my_port> <receptor_id> <receptor_port> <file_path> \n");
+        return 0;
     }
-
-		
-		other.sin_addr.s_addr = inet_addr(argv[3]); //IP destino se especifica en el 2 parametro de linea de comando
-		other.sin_port = htons(atoi(argv[4]));
-		
-		while(!end_flag)
-		{
-			package[0] = 0;
-			
-			fseek( file, batch_counter*10*512, SEEK_SET);
-			
-			int confirmed = 0;//cantidad de paquetes ya recividos(confirmados)
-			
-			for(int index = 0; index < current_batch_size; ++index)
-			{
-								
-				if( ack_chars[index] == 0 )//if ack recv hasn't arrived
-				{
-					char* tmp;
-					tmp = (char*)(&index);
-					package[1] = tmp[1];
-					package[2] = tmp[2];
-					package[3] = tmp[3];
-						
-					if(fread(package+4, 512, 1, file) < 1)
-					{
-						end_flag = 1;
-					}
-					
-					sendto(sockfd, package, PACK_SIZE, 0, (struct sockaddr*)&other, len);
-					
-					
-				}else{//if ack confirmation was recieved
-					
-					fseek( file, batch_counter*index*512, SEEK_SET);
-					++confirmed;	
-				}	
+    
+    FILE* file; 
+    file = fopen(argv[5], "rb");
+    
+    pid_t cpid; //create child id
+    
+    char package[PACK_SIZE];
+    int sockfd, sockfd2, n, len, current_batch_size;
+    int RN = 0;
+    int SN = 0;
+    struct sockaddr_in me, other;
+    int end_flag = 0;
+    
+    // Limpia el registro
+    bzero(&me, sizeof(me));
+    me.sin_addr.s_addr = inet_addr(argv[1]);
+    me.sin_port = htons(PORT);
+    me.sin_family = AF_INET;
+    other.sin_family = AF_INET;
+    
+    // create datagram socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    // bind created socket to my addres
+    bind(sockfd, (struct sockaddr *)&me, sizeof(me));
+    len = sizeof(other);
+    
+    cpid = fork();
+    if(cpid == -1)
+    {
+        perror("Fork failed to create child lul \n");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    //parent (emisor)
+    if(cpid != 0)
+    {
         
-        recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
-        if(package[0] == 1) 
+        other.sin_addr.s_addr = inet_addr(argv[3]); //IP destino se especifica en el 2 parametro de linea de comando
+        other.sin_port = htons(atoi(argv[4]));
+        
+        //queue_t q;
+        //q.init();
+        
+        while(!end_flag)
         {
-        		int seq_num;
-         		char tmp[4];
-            tmp[0] = 0;
-        		tmp[1] = package[1];
-          	tmp[2] = package[2];
-          	tmp[3] = package[3];
-          	seq_num = (int)tmp;
-          	
-          	ack_chars[seq_num] = 1;
+            package[0] = 0; //indicar que es un SN
+            
+            /*
+          char* tmp;
+          tmp = (char*)(&SN);
+          package[0];
+          package[1] = tmp[1];
+          package[2] = tmp[2];
+          package[3] = tmp[3];
+          */
+            
+            fseek( file, SN*10*512, SEEK_SET);
+            
+            for(int index = 0; index < 10; ++index)
+            {
+                package[0] = 0;
+                package[3] = index;
+                
+                if(fread( package+4, 512, 1, file) > 0)
+                {
+                    sendto(sockfd, package, PACK_SIZE, 0, (struct sockaddr*)&other, len);
+                }
+                else
+                {
+                    end_flag = 1;
+                }
+            }      
+            
+            usleep(800000); //time-out simulation
+            recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
+            
+            if(package[0] == 1) 
+            {
+                int seq_num;
+                char tmp[4];
+                tmp[0] = 0;
+                tmp[1] = package[1];
+                tmp[2] = package[2];
+                tmp[3] = package[3];
+                seq_num = (int)tmp;
+            }
+            
+            //manejo de cola
+            if( SN < seq_num )
+            {
+                SN = seq_num;
+            }
+            /* else
+          {
+            usleep(400000);
+            
+            recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
+          }*/
+            
         }
+        printf("Salí del ciclo\n");
+    }
+    
+    //child (reciever)
+    if(cpid == 0)
+    {
         
+        FILE* copypy;
+        file = fopen( copy_file , "wr" );
         
-			}			
-
-			if(confirmed == current_batch_size)
-			{
-				batch_counter++;
-        bzero(ack_chars, sizeof(int));
-        total_packages_sent += current_batch_size;
-			}
-     
-      
-      if( total_packages_sent == (total_package_counter - total_package_counter % 10 )  )//cuando solo faltan total_packages %10 por enviar,se envian los restantes de forma separada
-      {
-        current_batch_size = total_package_counter % 10;
-      }
-      
-      if( (current_batch_size != 10) && (total_packages_sent == total_package_counter))
-      {
-        end_flag = true;
-        package[0] = 0;
-        package[PACK_SIZE-1] = '*'; //PREGUNTAR
-        sendto(sockfd, package, PACK_SIZE, 0, (struct sockaddr*)&other, len);
-      }
-
-			usleep(500000);
-		}
-		
-    
-		printf("Salí del ciclo\n");
-	}
-  
-	//child (reciever)
-	if(cpid == 0)
-	{
-    
-    other.sin_addr.s_addr = inet_addr(argv[3]); //IP destino se especifica en el 2 parametro de linea de comando
-		other.sin_port = htons(atoi(argv[4]));
-				
-		recv_matrix = (char** )calloc(PACKAGE_LIMIT, sizeof(char* ));
-    
-
-    while(!end_flag)
-		{
-      
-        for(int index = 0; index < 10; ++index)
+        other.sin_addr.s_addr = inet_addr(argv[3]); //IP destino se especifica en el 2 parametro de linea de comando
+        other.sin_port = htons(atoi(argv[4]));
+        
+        queue_t q;
+        q.init;
+        
+        while(!end_flag)
         {
-          	int check_recv = recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
+            
+            int check_recv = recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
             if(check_recv > 0)
             {
-              	if(package[0] == 0)
+                if(package[0] == 0)
                 {
-                  	int seq_num;
+                    int seq_num;
                     char tmp[4];
-                  	tmp[0] = 0;
+                    tmp[0] = 0;
                     tmp[1] = package[1];
                     tmp[2] = package[2];
                     tmp[3] = package[3];
-                    seq_num = (int)tmp;
-
-                    ack_chars[seq_num] = 1;
-                  
-                  	strncpy(recv_matrix[seq_num],package+4,PACK_THROUGHPUT);
-                  	
+                    seq_num = (int)tmp;	
+                    
+                    //RN es el número de secuencia que el receptor está esperando
+                    //SN es el número de secuencia que el emisor está enviando actualmente
+                    if( seq_num <= RN + PACKAGE_LIMIT-1 )
+                    {
+                        q.enQueue(package);
+                    }	
+                    
+                    //strncpy(recv_matrix[seq_num],package+4,PACK_THROUGHPUT);//Dentro de la cola (hacer copia de los 512 bytes)
+                    
+                    strncpy(package, q.first(), 516);
+                    if( seq_num == RN )
+                    {
+                        fwrite( q.first+4 , 1 , PACK_THROUGHPUT , copy );
+                        q.dequeue();
+                        ++RN;
+                    }
+                    //q.first()->[0] = 1;
+                    package[0] = 1;//decir que es un RN
+                    sendto( sockfd, package ,PACK_SIZE, 0 (struct sockaddr*)&other, len );
                 }
+            } 	
+            
+            if( queue.first[516] == '*' ) //el último byte del jpg nunca puede ser '*', es el token de finalizar comunicación
+            {
+                end_flag = true;
             }
-          		
+            
+            usleep(400000);
+            
         }
-      
+        
+        
+        
     }
     
-		/*
-		other.sin_addr.s_addr = inet_addr(ip); //IP destino se especifica en el 2 parametro de linea de comando
-		other.sin_port = htons(atoi(other_port));
-				
-		recv_matrix = (char** )calloc(PACKAGE_LIMIT, sizeof(char* ));
-		while(!end_flag)
-		{
-			
-			recvfrom(sockfd, package, PACK_SIZE, MSG_DONTWAIT, (struct sockaddr*)&other, &len);
-			
-			int package_position = package[3]; //order of the package in the 10 package batch
-			
-			strncpy( recv_matrix[package_position], package+4, 512 );
-			
-			ack_chars[package_position] = 1;	
- 
-			sendto(sockfd, send, MAXLINE, 0, (struct sockaddr*)&other, len);
-			
-			
-				
-			usleep(300000);
-		}*/
-	}
-
-	close(sockfd);
-
+    close(sockfd);
+    
+    return 0;
+    
 }
